4513142228bcf451497572082a1eb7293c9953bf
May <may0220@yandex.ru>
2024-11-06 13:07:24 +0300

    when eat more then one afte eating last stop going

--- checkers/main.py
+++ checkers/main.py
@@ -40,10 +40,10 @@ WHITE_WIN = Fore.CYAN + 'White WIN!!! Congratulations!!!' + Style.RESET_ALL
 BOARD_DICT = {
               18: EMPTY_CELL, 28: BLACK_CHECKER_CELL, 38: EMPTY_CELL, 48: BLACK_CHECKER_CELL, 58: EMPTY_CELL, 68: BLACK_CHECKER_CELL, 78: EMPTY_CELL, 88: BLACK_CHECKER_CELL,
               17: BLACK_CHECKER_CELL, 27: EMPTY_CELL, 37: BLACK_CHECKER_CELL, 47: EMPTY_CELL, 57: BLACK_CHECKER_CELL, 67: EMPTY_CELL, 77: BLACK_CHECKER_CELL, 87: EMPTY_CELL,
-              16: EMPTY_CELL, 26: EMPTY_CELL, 36: EMPTY_CELL, 46: EMPTY_CELL, 56: EMPTY_CELL, 66: BLACK_CHECKER_CELL, 76: EMPTY_CELL, 86: BLACK_CHECKER_CELL,
-              15: EMPTY_CELL, 25: EMPTY_CELL, 35: BLACK_CHECKER_CELL, 45: EMPTY_CELL, 55: EMPTY_CELL, 65: EMPTY_CELL, 75: EMPTY_CELL, 85: EMPTY_CELL,
-              14: EMPTY_CELL, 24: EMPTY_CELL, 34: EMPTY_CELL, 44: EMPTY_CELL, 54: EMPTY_CELL, 64: EMPTY_CELL, 74: EMPTY_CELL, 84: EMPTY_CELL,
-              13: WHITE_CHECKER_CELL, 23: EMPTY_CELL, 33: WHITE_CHECKER_CELL, 43: EMPTY_CELL, 53: WHITE_CHECKER_CELL, 63: EMPTY_CELL, 73: WHITE_CHECKER_CELL, 83: EMPTY_CELL,
+              16: EMPTY_CELL, 26: EMPTY_CELL, 36: EMPTY_CELL, 46: EMPTY_CELL, 56: EMPTY_CELL, 66: EMPTY_CELL, 76: EMPTY_CELL, 86: BLACK_CHECKER_CELL,
+              15: EMPTY_CELL, 25: EMPTY_CELL, 35: BLACK_CHECKER_CELL, 45: EMPTY_CELL, 55: BLACK_CHECKER_CELL, 65: EMPTY_CELL, 75: EMPTY_CELL, 85: EMPTY_CELL,
+              14: EMPTY_CELL, 24: WHITE_CHECKER_CELL, 34: EMPTY_CELL, 44: EMPTY_CELL, 54: EMPTY_CELL, 64: EMPTY_CELL, 74: EMPTY_CELL, 84: EMPTY_CELL,
+              13: WHITE_CHECKER_CELL, 23: EMPTY_CELL, 33: EMPTY_CELL, 43: EMPTY_CELL, 53: WHITE_CHECKER_CELL, 63: EMPTY_CELL, 73: WHITE_CHECKER_CELL, 83: EMPTY_CELL,
               12: EMPTY_CELL, 22: WHITE_CHECKER_CELL, 32: EMPTY_CELL, 42: WHITE_CHECKER_CELL, 52: EMPTY_CELL, 62: WHITE_CHECKER_CELL, 72: EMPTY_CELL, 82: WHITE_CHECKER_CELL,
               11: WHITE_CHECKER_CELL, 21: EMPTY_CELL, 31: WHITE_CHECKER_CELL, 41: EMPTY_CELL, 51: WHITE_CHECKER_CELL, 61: EMPTY_CELL, 71: WHITE_CHECKER_CELL, 81:  EMPTY_CELL
 }
@@ -132,13 +132,14 @@ class Checker:
                 self.return_staps_and_checkers_dict[2*stap - checker] = stap # {stap: eating_checker}
 
     def get_possible_staps(self):
+        self.return_staps_and_checkers_dict = {}
         self.check_can_eat(self.int_checker)
 
     def make_stap(self, stap):
         if len(self.return_staps_and_checkers_dict) != 0:
             self.board[self.return_staps_and_checkers_dict[stap]] = EMPTY_CELL
         self.board[self.int_checker] = EMPTY_CELL
-        self.return_staps_and_checkers_dict = {}
+        # self.return_staps_and_checkers_dict = {}
 
     def check_can_be_queen(self, stap):
         if stap in self.QUEEN_CONDITION:
@@ -387,10 +388,12 @@ class Game:
         temp_checker = 0
         stap = self.check_inp_stap(possible_staps_and_eating_checkers_dict)
         checker.make_stap(stap)
-        print(checker.return_staps_and_checkers_dict, checker)
+        print(1)
         if len(checker.return_staps_and_checkers_dict) != 0:
             checker.check_can_eat(stap)
+            print(2, checker.return_staps_and_checkers_dict)
             if len(checker.return_staps_and_checkers_dict) != 0:
+                print(3)
                 if type(checker) == WhiteChecker:
                     temp_checker = WhiteChecker(stap, now_board.return_board())
                 elif type(checker) == BlackChecker:
@@ -437,11 +440,12 @@ class Game:
         temp_now_board = copy.deepcopy(now_board)
         self.possible_staps_and_eating_checkers_for_all_checkers_dict = self.check_can_eat_for_all_checkers(temp_now_board.return_board())
         if len(self.possible_staps_and_eating_checkers_for_all_checkers_dict) == 0:
-            return now_board
+            now_board.show()
         else:
             for ch in self.possible_staps_and_eating_checkers_for_all_checkers_dict:
                 temp_now_board.make_possible_staps(self.possible_staps_and_eating_checkers_for_all_checkers_dict[ch])
-            return temp_now_board
+            now_board.show()
+            temp_now_board.show()
 
     def run(self):
         while self.one_more:
@@ -449,7 +453,7 @@ class Game:
             self.iteration = 2  # iteration%2 == 0 go white, iteration%2 != 0 - go black
             while self.running:
                 now_board = Board(board)
-                self.show_possible_staps_for_all_checkers(now_board).show()
+                self.show_possible_staps_for_all_checkers(now_board)
                 self.who_go()
                 out_int_checker = self.check_inp_checker(now_board.return_board())
                 checker = self.create_checker(out_int_checker, now_board.return_board())
